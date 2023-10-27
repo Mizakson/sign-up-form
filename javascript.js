3325f02b0ba946781f6e84d9c7e18c7d23a18442
Mizakson <maxwellizakson@gmail.com>
2023-10-27 12:29:49 -0400

    Create basic js functions, edit styles formatting

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function getPasswords(pw1, pw2) {
+    return pw1, pw2;
+};
+
+function validatePasswords(pw1, pw2) {
+    if (pw1 === pw2) {
+        alert('Passwords match');
+    } else {
+        alert('Passwords do not match');
+    }
+}
+
