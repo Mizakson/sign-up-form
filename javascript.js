df1310ce76c45caf49d1c74e76491032eab2ae53
Mizakson <maxwellizakson@gmail.com>
2023-10-31 11:59:55 -0400

    Finish project
    Add password validations to index
    
    Change input font-family -> inherit
    
    Clean up javascript file, remove previous validation attempts

--- javascript.js
+++ javascript.js
@@ -1,12 +1,35 @@
-function getPasswords(pw1, pw2) {
+//DOM ELEMENTS
+const firstPassword = document.getElementById('pword');
+const secondPassword = document.getElementById('confirm-pword');
+const submitButton = document.querySelector('button');
+const passwords = document.querySelectorAll('.pword');
+
+let pw1 = "";
+let pw2 = "";
+
+function logPasswords() {
+    
+    firstPassword.addEventListener('input', (e) => {
+        pw1 = e.target.value;
+        console.log('pw1: '+pw1);
+    })
+    
+    secondPassword.addEventListener('input', (e) => {
+        pw2 = e.target.value;
+        console.log('pw2: '+pw2);
+    })
+    
     return pw1, pw2;
+
 };
 
-function validatePasswords(pw1, pw2) {
-    if (pw1 === pw2) {
-        alert('Passwords match');
-    } else {
-        alert('Passwords do not match');
-    }
+
+function formCompletion() {
+    submitButton.addEventListener('click', () => {
+        alert('Form submitted successfully!')
+        console.log('form submission');
+    })
 }
 
+logPasswords();
+formCompletion();
